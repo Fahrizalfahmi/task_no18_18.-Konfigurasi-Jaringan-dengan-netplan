bf2cd5fe105e27d2f3e9b0f5ebb5710ab08036e6
Fahrizal Fahmi <fahrizalfahmi44@gmail.com>
2024-06-18 22:24:29 +0700

    Add files via upload

---
+++ task18.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cat <<EOT > /etc/netplan/01-netcfg.yaml
+network:
+  version: 2
+  renderer: networkd
+  ethernets:
+    eth0:
+      addresses: [192.168.1.100/24]
+      gateway4: 192.168.1.1
+      nameservers:
+        addresses: [8.8.8.8, 8.8.4.4]
+EOT
+
+netplan apply
+
